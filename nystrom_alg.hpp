34f729c070f4aa56138426da9405ce88c17dd32a
Sameer Tharakan <tharakan@login3.stampede.tacc.utexas.edu>
2015-03-30 18:43:58 -0500

    Finished writing functions for decomposition

--- nystrom_alg.hpp
+++ nystrom_alg.hpp
@@ -6,12 +6,17 @@
 #include "El.hpp"
 #include "kernel_inputs.hpp"
 #include "nystrom_utils.hpp"
+#include "gaussKernel.hpp"
+#include "clustering.h"
 //#include "parallelIO.h"
 
 #include <iostream>
 #include <time.h>
+#include <vector>
+#include <math.h>
 
 using namespace El;
+
 
 //template<class TKernel>
 class NystromAlg {
@@ -30,7 +35,7 @@ public:
 	DistMatrix<double,VR,STAR> L;
 
 	// Reordering permutation
-	DistMatrix<double,VR,STAR> permute;
+	DistMatrix<int,VR,STAR> permute; //TODO is this needed?
 
 	// U matrix for the sample points (truncated to reduced rank)
 	DistMatrix<double> U; 
@@ -44,34 +49,39 @@ public:
   * - kernel_inputs -- kernel input parameter
   * - nystrom_inputs -- parameters like nystrom rank
   */
-  NystromAlg(DistMatrix<double>* _refData, KernelInputs& _kernel_inputs, NystromInputs& _nystrom_inputs,Grid* _g); 
+  NystromAlg(DistMatrix<double>* _refData, KernelInputs& _kernel_inputs, NystromInputs& _nystrom_inputs,Grid* _g, GaussKernel _gKernel); 
   
   ~NystromAlg(); //TODO -- need to free non-matrix variables
  
   /*
    * Performs nystrom decomp, returning values into U, L, and permutation
    */
-  void decomp(); //TODO
+  void decomp(); 
 
 	/*
 	 * Orthogonalizes the system, stores result in K_nm (along with modified L)
 	 */
-	void orthog(); //TODO
+	void orthog(); 
 
   /*
    * Performs a matrix vector multiply. User must create weight
 	 * vector and allocate space for the result of the multiply
    */
-	//DistMatrix<double> matvec(DistMatrix<double>& weights); //TODO
-	void matvec(DistMatrix<double>& weights); //TODO
+	void matvec(DistMatrix<double>& weights,DistMatrix<double>& out); 
 
 	/*
 	 * Applies the inverse to a vector and returns the output. As
 	 * with matvec, user must create rhs and allocate memory for 
 	 * the result
 	 */
-	//DistMatrix<double> appinv(DistMatrix<double>& rhs); //TODO
-	void appinv(DistMatrix<double>& rhs); //TODO
+	void appinv(DistMatrix<double>& rhs, DistMatrix<double>& x); 
+
+	/*
+	 * Computes the Gaussian kernel from a set of points when compared to itself.
+	 * The inputs are specified in KernelInputs
+	 */
+	void self_GaussKernel(DistMatrix<double>& data, DistMatrix<double>& K); 
+
 
 private:
 
@@ -93,16 +103,31 @@ private:
 	// Nystrom inputs: record them here for ease
 	int nystrom_rank;
 	int nystrom_samples;
-	
+
+	// Pointers to data
+	DistMatrix<double>* ptrX;
+	DistMatrix<double>* ptrY;
+
+	// Kernel
+	GaussKernel gKernel;
+
+	// Make a class for this later
+	void kernelCompute(Matrix<double>& sources, Matrix<double>& targets, Matrix<double>& kernel);
+
+	// Store all the indices we'll need
+	std::vector<int> s_idx,l_idx,d_idx,dummy_idx;
+
 }; // class
 
-NystromAlg::NystromAlg(DistMatrix<double>* _refData, KernelInputs& _kernel_inputs, NystromInputs& _nystrom_inputs,Grid* _g):
+NystromAlg::NystromAlg(DistMatrix<double>* _refData, KernelInputs& _kernel_inputs, NystromInputs& _nystrom_inputs,Grid* _g, GaussKernel _gKernel):
 	refData(_refData),
 	kernel_inputs(_kernel_inputs),
 	nystrom_inputs(_nystrom_inputs),
-	g(_g)
+	g(_g),
+	gKernel(_gKernel)
 {
-	
+	ptrX = refData;
+	ptrY = refData;
 	ntrain          = refData->Height();
 	dim             = refData->Width();
 	dcmp_flag       = false;
@@ -110,11 +135,19 @@ NystromAlg::NystromAlg(DistMatrix<double>* _refData, KernelInputs& _kernel_input
 	nystrom_rank    = nystrom_inputs.rank;
 	nystrom_samples = nystrom_inputs.samples;
 
-
 	L.SetGrid(*g);
 	permute.SetGrid(*g);
 	U.SetGrid(*g);
 	K_nm.SetGrid(*g);
+	
+	l_idx.resize(nystrom_samples);
+	s_idx.resize(nystrom_rank);
+	d_idx.resize(dim);
+	dummy_idx.resize(1);
+	dummy_idx[1] = 0;
+	for(int i=0;i<nystrom_samples;i++){ l_idx[i] = i;} //TODO omp this loop
+	for(int i=0;i<nystrom_rank;i++){ s_idx[i] = i;} //TODO omp this loop
+	for(int i=0;i<dim;i++){ d_idx[i] = i;} //TODO omp this loop
 
 	// Allocate memory or at least try to
 	try{
@@ -138,49 +171,209 @@ NystromAlg::~NystromAlg(){
 	// Flags
 }
 	
+	
 void NystromAlg::decomp(){
 		if (!dcmp_flag){
 			// Random sample of size nystrom_samples
-			//std::vector<Int> smpIdx();	
+			std::vector<Int> smpIdx(nystrom_samples);	
+	
+			//TODO Share among processes?
+			if(mpi::WorldRank() == 0){
+				std::vector<int> _smpIdx;
+				randperm(nystrom_samples,ntrain,_smpIdx);
+				std::cout << "Sample idx" << std::endl;
+				for (int i=0;i<nystrom_samples;i++){
+					std::cout << _smpIdx[i] << std::endl;
+					smpIdx[i] = _smpIdx[i];
+				}
+				_smpIdx.clear();
+			}
+
+			//Send vector to everybody else
+			mpi::Broadcast(&smpIdx[0], nystrom_samples, 0, mpi::COMM_WORLD);
 			
-			// Fill with kernel values
-			//Matrix<double> K_mm(nystrom_samples,nystrom_samples,*g);
-
-
-			// Take SVD of subsampled matrix
-			//Matrix<double> dummy(*g);
-			//Matrix<double> s(*g);
-			//SVD(K_mm,dummy,s);
-
-			//TODO Do we need to do this?
-			// Sort singular values, store permutation
+			//int A[4] = {1,0,0,1};
+			//std::vector<double> A_vec(A,A+4);
+			//if(mpi::WorldRank() ==0){
+			//	for (int i=0; i<A_vec.size();i++){
+			//		std::cout <<A_vec[i]<<std::endl;
+			//	}
+			//}
 			
+			// Sample from data
+			DistMatrix<double> Xsub(*g);
+			if(mpi::WorldRank() ==0){
+				std::cout << "Full idx (dim)" <<std::endl;
+				for(int i =0;i<d_idx.size();i++){
+					std::cout << d_idx[i] <<std::endl;
+				}
+			}
+			GetSubmatrix(*ptrX,smpIdx,d_idx,Xsub);
+			Print(Xsub,"X_mn");	
+			// Fill K_mm with kernel values 
+			DistMatrix<double> K_mm(nystrom_samples,nystrom_samples,*g);
+			gKernel.SelfKernel(Xsub, K_mm);
+			Print(K_mm,"K_mm");
+			//fill with rands for now //TODO take out
+			//Uniform(K_mm,nystrom_samples,nystrom_samples,0.25,0.25);
+			//DistMatrix<double> mmCopy(*g);
+			//FillDiagonal(K_mm,0.5);
+			//Copy(K_mm,mmCopy);
+			//AdjointAxpy(1.0,mmCopy,K_mm);
+			//mmCopy.Empty();
+			
+			// Take Eigendecomp of subsampled matrix
+			auto mmCopy(K_mm);
+			DistMatrix<double> Umm(*g);
+			DistMatrix<double> Lmm(*g);
+			HermitianEig(UPPER,mmCopy,Lmm,Umm,DESCENDING);
+			mmCopy.Empty();
+
+			// This is just because we cant read yet //TODO take out
+			//double minL = Min(Lmm).value;
+			//if(minL < 0.0){
+			//	DistMatrix<double> Lcorr(nystrom_samples,1,*g);
+			//	Fill(Lcorr,1.01);
+			//	Axpy(-minL, Lcorr, Lmm);
+			//}
+			
+			// Truncate
+			GetSubmatrix(Umm,l_idx,s_idx,U);
+			GetSubmatrix(Lmm,s_idx,dummy_idx,L);
+			Umm.Empty();
+			Lmm.Empty();
 
 			// Compute K_nm
+			gKernel.Kernel(*ptrX,Xsub,K_nm);	
 			
 			dcmp_flag = true;
 		}
 		else{
-			std::cout << "Decomposition performed!" << std::endl;
+			if(mpi::WorldRank() == 0){std::cout << "Decomposition already performed!" << std::endl;}
 		}
 }
 
 void NystromAlg::orthog(){
+	// If we haven't done decomp, do that now
+	if(!dcmp_flag){this->decomp();}
 
+	// Check if we've already orthogonalized
+	if(!orth_flag){
+		K_nm.EmptyData(); //Don't need this until later
+		
+		//Form the large U (KU below)
+		DistMatrix<double> KU(ntrain,nystrom_rank,*g);
+		Fill(KU,0.0);
+		Uniform(K_nm,ntrain,nystrom_samples,0.5,0.5);
+		Gemm(NORMAL,NORMAL,1.0,K_nm,U,1.0,KU);
 
+		//Take the QR
+		DistMatrix<double,MD,STAR> t(*g);
+		DistMatrix<double,MD,STAR> d(*g);
+		QR(KU,t,d);
+
+		//Form R L R^T
+		DistMatrix<double> R(*g);
+		GetSubmatrix(KU,s_idx,s_idx,R); 
+		MakeTrapezoidal(UPPER,R);
+		auto elem_sqrt = [](double x){return (sqrt(x));};//TODO check L is nonneg before calling this?
+		EntrywiseMap(L,function<double(double)>(elem_sqrt));
+		DiagonalScaleTrapezoid(RIGHT,UPPER,NORMAL,L,R); // Now R_curr = R_true L^(1/2) 
+		Trtrmm(UPPER,R); // Now R_curr = R_old R_old^T  = R_true L R_true
+
+		// Eigendecompose that product
+		auto RLRt(R);
+		DistMatrix<double> smallQ(*g);
+		DistMatrix<double> newL(*g);
+		HermitianEig(UPPER,R,newL,smallQ,DESCENDING);
+		
+		// Restrict Q
+		DistMatrix<double> Q_nm(*g);
+		Identity(Q_nm,ntrain,nystrom_rank);
+		qr::ApplyQ(LEFT,NORMAL,KU,t,d,Q_nm);
+		KU.Empty();
+		t.Empty();
+		d.Empty();
+		
+		// Load combined Q into K_nm
+		K_nm.Resize(ntrain,nystrom_rank);
+		Fill(K_nm,0.0);
+		Gemm(NORMAL,NORMAL, 1.0,Q_nm,smallQ, 1.0,K_nm);
+		smallQ.Empty();
+		Q_nm.Empty();
+		
+		//Load new L into L
+		L = newL;
+		newL.Empty();
+
+		// Let everyone else know, also don't need U anymore --> dont know if 
+		// emptying it helps
+		orth_flag = true;
+		//U.Empty();
+	}
+	else{
+		if(mpi::WorldRank() == 0){std::cout << "Already orthogonalized!"<<std::endl;}
+	}
 
 }
 
-void NystromAlg::matvec(DistMatrix<double>& weights){
+void NystromAlg::matvec(DistMatrix<double>& weights, DistMatrix<double>& out){
+	
+	// IF we have orthogonalized, out = K_nm L K_nm^T weights
+	// IF we have only decomped,  out = K_nm U L U^T K_nm^T weights
+	// Either way, can do Kw = K_nm^T * w
+	DistMatrix<double, VR, STAR> Kw(ntrain,1,*g);
+	Fill(Kw,0.0);
+	Gemv(TRANSPOSE,1.0,K_nm,weights,1.0,Kw);
+	
+	// Apply either just L, or K_mm = U L U^T, store output in Kw
+	if(!orth_flag){
+		DistMatrix<double,VR, STAR> dummy(nystrom_rank,1,*g);
+		Fill(dummy,0.0);
+		Gemv(TRANSPOSE,1.0,U,Kw,1.0,dummy);
+		Fill(Kw,0.0);
+		DiagonalScale(RIGHT,NORMAL,L,dummy);
+		Gemv(NORMAL,1.0,U,dummy,1.0,Kw);
+		dummy.EmptyData();
+	}
+	else{
+		DiagonalScale(RIGHT,NORMAL,L,Kw);
+	}
+	
+	// Set up output vector properly
+	out.Resize(ntrain,1);
+	Fill(out,0.0);
 
-	
-	
+	// Finish by applying K_nm
+	Gemv(NORMAL,1.0,K_nm,Kw,1.0,out);
 }
 
-void NystromAlg::appinv(DistMatrix<double>& rhs){
+void NystromAlg::appinv(DistMatrix<double>& rhs, DistMatrix<double>& x){
+	// Make sure it is orthogonalized
+	if(!orth_flag){
+		if(mpi::WorldRank() == 0){
+			std::cout << "Need to orthogonalize first .." << std::endl;
+		}
+		this->orthog();
+	}
+	
+	// Kapprox = K_nm L K_nm^T, so just need to invert diag
+	// since K_nm is orthogonal
+	// 
+	DistMatrix<double,VR,STAR> Kw(nystrom_rank,1,*g);
+	Fill(Kw,0.0);
+	Gemv(TRANSPOSE, 1.0,K_nm,rhs, 1.0,Kw);
 
+	// Scale by inv diag
+	DiagonalSolve(RIGHT,NORMAL,L,Kw);
 
-
+	// Finish multiply, load into x
+	x.Resize(nystrom_rank,1);
+	Fill(x,0.0);
+	Gemv(TRANSPOSE, 1.0,K_nm,Kw, 1.0,x);
+	
+	// Free the dummy vector
+	Kw.Empty();
 }
 
 #endif
--- nystrom_main.cpp
+++ nystrom_main.cpp
@@ -1,24 +1,22 @@
 
 #include "nystrom_alg.hpp"
-#include <iostream>
 
 using namespace El;
 
 int main(int argc, char* argv []){
 	Initialize(argc,argv);
+	Int ntrain = 20;
+	Int dim = 10;
+	
+	int mpicomms = mpi::Size(mpi::COMM_WORLD);
+	std::cout << "Creating grid on " << mpicomms << " mpi tasks"<<std::endl;
+	Grid grid(mpi::COMM_WORLD);
 
 	try{
-		Int ntrain = 50;
-		Int dim = 50;
-	
-		std::cout << "Creating grid" <<std::endl;
-		Grid grid(mpi::COMM_WORLD);
-	
-
-		std::cout << "Initializing data" <<std::endl;
+		//std::cout << "Initializing data" <<std::endl;
 		DistMatrix<double> refData(ntrain,dim,grid);
 
-		std::cout << "Loading data" <<std::endl;
+		//std::cout << "Loading data" <<std::endl;
 		//Uniform(*refData,ntrain,dim);
 		for(Int i=0;i<ntrain;i++){
 			for(Int j =0;j<dim;j++){
@@ -26,22 +24,84 @@ int main(int argc, char* argv []){
 				refData.Set(i,j, (double) (i+j));
 			}
 		}
-
-		std::cout << "Loading kernel params" <<std::endl;
+		
+		//std::cout << "Loading kernel params" <<std::endl;
 		KernelInputs kernel_inputs;
-		kernel_inputs.bandwidth = 1;
+		kernel_inputs.bandwidth = 10;
 
-		std::cout <<"Loading nystrom params" <<std::endl;
-		NystromInputs nystrom_inputs(10);
-  	
-		std::cout << "Initializing NystromAlg obj" <<std::endl;
-		NystromAlg nyst(&refData,kernel_inputs,nystrom_inputs,&grid); 
+		//std::cout <<"Loading nystrom params" <<std::endl;
+		NystromInputs nystrom_inputs(6);
+  
+		mpi::Barrier(mpi::COMM_WORLD);
+		//std::cout << "Making kernel class" <<std::endl;
+		GaussKernel gKernel(kernel_inputs, &grid);
+
+		//std::cout << "Initializing NystromAlg obj" <<std::endl;
+		NystromAlg nyst(&refData,kernel_inputs,nystrom_inputs,&grid, gKernel);
+
+		//std::cout << "Running decomp" <<std::endl;
+		nyst.decomp();
+
+		//std::cout << "Running orthog" <<std::endl;
+		nyst.orthog();
+
+		DistMatrix<double> test_vec(grid);
+		Uniform(test_vec,ntrain,1);
+		DistMatrix<double> ans(grid);
+		
+		//std::cout << "Testing multiply" <<std::endl;
+		nyst.matvec(test_vec,ans);
+
+		//std::cout << "Testing appinv" << std::endl;
+		ans.Empty();
+		nyst.appinv(test_vec,ans);
+	
+		/*	
+		// Trying to write kernel business
+		DistMatrix<double> A(grid);
+		int height = 4; int width = 4;
+		Uniform(A,height,width);
+		Print(A, "Data");
+
+		DistMatrix<double> K(width,width,grid);
+		Fill(K,0.0);
+		Herk(UPPER,NORMAL, -2.0,A, 1.0,K);
+
+		auto elem_sqr = [](double x){return x*x;};
+		EntrywiseMap(A,function<double(double)> (elem_sqr));
+		Print(A,"Ptwise sqr data");
+			
+		DistMatrix<double,VR,STAR> ones(width,1,grid);
+		Fill(ones,1.0);
+		
+		DistMatrix<double,VR,STAR> norms(height,1,grid);
+		Fill(norms,0.0);
+
+		Gemv(NORMAL, 1.0,A,ones, 1.0,norms);
+		Print(norms, "norms");
+		
+		// Need to combine it all back
+		ones.Resize(height,1);
+		Fill(ones,1.0);
+		Print(ones,"ones");
+		Her2(UPPER, 1.0,ones,norms, K);
+		auto elem_sqrt = [](double x){return sqrt(x);};
+		EntrywiseMap(K,function<double(double)> (elem_sqrt));
+
+		Print(K,"unexponentiated kernel");
+		
+		auto elem_exp = [](double x){return exp(x);};
+		Scale(-1.0,K);
+		EntrywiseMap(K,function<double(double)>(elem_exp));
+		Print(K,"final kernel (upper)");
+		*/
 
 
+		//Print(A);
 	}
 	catch(exception& e){ ReportException(e); }
-
 	Finalize();
+
 	return 0;
 }
 
