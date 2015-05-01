b536a63e2c5fdf1f9cd9e655aecad51d79ab1766
Sameer Tharakan <tharakan@login3.maverick.tacc.utexas.edu>
2015-05-01 09:36:20 -0500

    added more tests, still can't find runtime error related to matvec

--- nystrom_main.cpp
+++ nystrom_main.cpp
@@ -8,6 +8,109 @@ using namespace El;
 using std::string;
 
 /*
+ * Tests orthog by checking the value of L*L-1
+ */
+double test_nystl(NystromAlg& nyst, double & cond){
+	const Grid& g = nyst.K_nm.Grid();
+	int height = nyst.L.Height();
+
+	// Get conditioning
+	double sig1 = nyst.L.Get(0,0);
+	double sigh = nyst.L.Get(height-1,0);
+	if(mpi::WorldRank() ==0){std::cout<< sig1<<" vs "<<sigh<<std::endl;}
+	cond = sig1/sigh;
+
+	// Test action instead of L L ^-1
+
+	// Make eye
+	/*
+	DistMatrix<double,VR,STAR> ones(height,1,g);
+	Fill(ones,1.0);
+
+	// Do multiply
+	auto testL(nyst.L);
+	DiagonalSolve(LEFT,NORMAL,nyst.L,testL);
+
+	// Get norm
+	Axpy(-1.0,ones,testL);
+	double d_err = FrobeniusNorm(testL);
+	return d_err/sqrt(height);
+	*/
+
+	// Test action instead of L L ^-1
+	DistMatrix<double,VR,STAR> vec(g);
+	Uniform(vec,height,1);
+	auto ref(vec);
+	double base_norm = FrobeniusNorm(vec);
+	DiagonalSolve(LEFT,NORMAL,nyst.L,vec);
+	DiagonalScale(LEFT,NORMAL,nyst.L,vec);
+	Axpy(-1.0,vec,ref);
+	double err = FrobeniusNorm(ref);
+	return err/base_norm;
+
+}
+/*
+ * Tests nyst by checking if U^T U = I
+ * should be called before orthogonalization 
+ */
+double test_nystu(NystromAlg& nyst){
+	const Grid& g = nyst.U.Grid();
+
+	// Make copy
+	auto V(nyst.U);
+	auto S(nyst.L);
+	int w = V.Width();
+	DiagonalScale(RIGHT,NORMAL,S,V);
+
+	// Make identity and multiply
+	DistMatrix<double> I(g);
+	Identity(I,w,w);
+
+	// Do multiply and subtract
+	Herk(UPPER,ADJOINT,1.0,V, -1.0,I);
+
+	// Get norm
+	double err = HermitianFrobeniusNorm(UPPER,I);
+	return err/sqrt(w);
+}
+/*
+ * Tests nyst by checking if ULU^T*K=I
+ * should be called before orthogonalization 
+ */
+double test_nysteig(NystromAlg& nyst){
+	const Grid& g = nyst.U.Grid();
+
+	// Make copy
+	auto V(nyst.U);
+	auto S(nyst.L);
+	int h = V.Height();
+
+	// Make kernel matrix
+	DistMatrix<double> K(h,h,g);
+	DistMatrix<double> Xsub;
+	GetSubmatrix(*(nyst.ptrX),nyst.get_d(),nyst.get_smp(),Xsub);
+	nyst.gKernel.SelfKernel(Xsub,K);
+	auto Kstar(K);
+	double base_norm = HermitianFrobeniusNorm(UPPER,K);
+
+	// Do multiply and subtract
+	DiagonalScale(RIGHT,NORMAL,S,V);
+	auto elem_sqrt = [](double x){return (sqrt(x));};
+	EntrywiseMap(S,function<double(double)>(elem_sqrt));
+	DiagonalScale(RIGHT,NORMAL,S,V);
+	Herk(UPPER,NORMAL,1.0,V, -1.0,K);
+
+	// Make identity and multiply
+	//DistMatrix<double> I(g);
+	//Identity(I,h,h);
+	//Gemm(NORMAL,NORMAL,1.0,Kstar,K, -1.0,I);
+
+	// Get norm
+	double err = HermitianFrobeniusNorm(UPPER,K);
+	return err/base_norm;
+}
+
+/*
  * Tests orthog by checking the value of D*D-1
  */
 double test_orthd(NystromAlg& nyst){
@@ -53,7 +156,7 @@ double test_ortheye(NystromAlg& nyst){
 	Herk(UPPER,ADJOINT,1.0,nyst.K_nm, -1.0,I);
 
 	// Get norm
-	double eye_err = FrobeniusNorm(I);
+	double eye_err = HermitianFrobeniusNorm(UPPER,I);
 	return eye_err/sqrt(width);
 }
 
@@ -176,7 +279,8 @@ int main(int argc, char* argv []){
 	// Set flags to do things if we want
 	const bool regression  = Input("--rr","do regression?",false);
 	const bool do_exact    = Input("--ex","compute reg mv exactly?",true);
-	const bool do_tests    = Input("--tt","do tests?",false);
+	const bool do_rtests    = Input("--tr","do regression tests?",false);
+	const bool do_ntests    = Input("--tn","do nystrom tests?",false);
 
 	// Finish up with inputs
 	ProcessInput();
@@ -200,7 +304,15 @@ int main(int argc, char* argv []){
 	string trdata = datadir;
 	trdata.append(trdataloc);
 	Read(Xtrain,trdata,BINARY_FLAT);
-
+/*
+	std::vector<Int> smpIdx(nyst_samp);
+	std::vector<Int> d_idx(dim);
+	for(int i=0;i<nyst_samp;i++){smpIdx[i]=i;}
+	for(int i=0;i<dim;i++){d_idx[i]=i;}
+	DistMatrix<double> Xsub(grid);
+	GetSubmatrix(Xtrain,d_idx,smpIdx,Xsub); 
+	Print(Xsub,"X_sub -- read");	
+*/
 	// Read labels
 	DistMatrix<double,VR,STAR> Ytrain(grid);
 	if(trlabloc.compare("") != 0){
@@ -293,7 +405,7 @@ int main(int argc, char* argv []){
 	nyst.decomp();
 	double decomp_time = mpi::Time() - start;
 
-	// Get timing
+	// Get timing 
 	double max_decomp_time;
 	mpi::Reduce(&decomp_time,&max_decomp_time,1,mpi::MAX,0,mpi::COMM_WORLD);
 	if(proc==0){std::cout << "Decomposition time   : " << max_decomp_time <<std::endl;}
@@ -321,8 +433,32 @@ int main(int argc, char* argv []){
 	//////////////////////////////
 	// ----- Decomp tests ----- //
 	//////////////////////////////
-	if(do_tests){
-		if(proc==0){std::cout << "Running tests ... " <<std::endl;}
+	if(do_ntests){
+		if(proc==0){std::cout << "Running nyst tests ... " <<std::endl;}
+
+		// Test for conditioning of diag L
+		double cond;
+		double nyst_ll_err = test_nystl(nyst, cond);
+		if(proc==0){std::cout << "Error from nyst ll^-1: " << nyst_ll_err <<std::endl;}
+		if(proc==0){std::cout << "Conditioning of L    : " << cond <<std::endl;}
+		
+		// Test viability of eigendecomp in nyst
+		double nyst_utu_err = test_nystu(nyst);
+		if(proc==0){std::cout << "Error from nyst uTu  : " << nyst_utu_err <<std::endl;}
+		
+		// Test viability of eigendecomp in nyst
+		double nyst_eig_err = test_nysteig(nyst);
+		if(proc==0){std::cout << "Error from nyst eig  : " << nyst_eig_err <<std::endl;}
+
+	}
+	//////////////////////////////
+	
+	
+	//////////////////////////////
+	// ------ Orth tests ------ //
+	//////////////////////////////
+	if(do_rtests){
+		if(proc==0){std::cout << "Running orth tests ... " <<std::endl;}
 
 		// Test multiply before and after orth
 		double orth_mv_err = test_orthmv(nyst);
@@ -332,17 +468,14 @@ int main(int argc, char* argv []){
 		double orth_err = test_ortheye(nyst);
 		if(proc==0){std::cout << "How close to orth?   : " << orth_err << std::endl;}
 
-		// Test for orthogonality of k_nm
+		// Test for conditioning of diag D
 		double diag_err = test_ortheye(nyst);
 		if(proc==0){std::cout << "Diag^-1 Diag err     : " << diag_err << std::endl;}
 		
 		// Test appinv
 		double inv_err = test_appinv(nyst);
 		if(proc==0){std::cout << "Error from appinv    : " << inv_err <<std::endl;}
-
-		// Put other tests here
-
-
+	
 	}
 	//////////////////////////////
 	
